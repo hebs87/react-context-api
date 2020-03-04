69548d9b3ebfb2bacfa13d998b5ebf0e8fddf545
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2020-03-04 13:16:39 +0000

    Moved shop.data.js file into collections context dir and amended dependencies in other files

--- src/contexts/collections/collections.context.js
+++ src/contexts/collections/collections.context.js
@@ -5,7 +5,7 @@
 import { createContext } from 'react';
 // Import SHOP_DATA as we want to set this as
 // our context's initial state
-import SHOP_DATA from "../../redux/shop/shop.data";
+import SHOP_DATA from "./shop.data";
 
 // We create a new context that holds the SHOP_DATA
 // which will be the initial value of our context
